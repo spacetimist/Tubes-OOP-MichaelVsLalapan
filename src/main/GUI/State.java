c0f81b8c91244cc5e8881a255beaef8d9f19c2df
rvna1104 <limkaervina@gmail.com>
2024-05-19 21:09:37 +0700

    Menu

--- src/main/GUI/State.java
+++ src/main/GUI/State.java
@@ -222,16 +222,63 @@ public class State {
 
         else if(menuScreenState == 1) {
             g2.setColor(Color.WHITE);
+            String text = "HELP";
+            int x = centerX(text);
+            int y = 80;
+            g2.drawString(text, x, y);
+            x = 110;
+            y += 50;
+            g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 15F));
+            g2.setColor(Color.YELLOW);
+            g2.drawString("Roro Jonggrang diserang oleh pasukan jin yang menjadi zombie.", x, y);
+            x = 130;
+            y += 20;
+            g2.drawString("Bandung Bondowoso ingin menyelamatkan istri tercintanya.", x, y);
+            x = 65;
+            y += 20;
+            g2.drawString("Ia memiliki benih tanaman berkemampuan khusus tetapi bingung mencari strategi.", x, y);
+            x = 135;
+            y += 20;
+            g2.setColor(Color.RED);
+            g2.drawString("Bantulah Bandung Bondowoso untuk menyusun strategi!!", x, y);
 
+            g2.setColor(Color.WHITE);
+            x = 265;
+            y += 40;
+            g2.drawString("Aturan permainan", x, y);
+            x = 125;
+            y += 20;
+            g2.drawString("1. Start game", x, y);
+            y += 20;
+            g2.drawString("2. Pilih dan masukkan tanaman dari inventory ke deck", x, y);
+            y += 20;
+            g2.drawString("3. Press 's' dan permainan dimulai", x, y);
+            y += 20;
+            g2.drawString("4. Tanam tanaman yang ada di deck ke dalam map", x, y);
+            y += 20;
+            g2.drawString("5. Terus menanam hingga zombie mati dan permainan selesai", x, y);
+
+            x = 300;
+            y += 40;
+            g2.drawString("Command", x, y);
+            x = 125;
+            y += 20;
+            g2.drawString("1. 'ESC' untuk keluar dari menu help, plants list dan zombies list", x, y);
+            y += 20;
+            g2.drawString("2. 'i' untuk pindah dari deck ke inventory", x, y);
+            y += 20;
+            g2.drawString("3. 'd' untuk pindah dari inventory ke deck", x, y);
+            y += 20;
+            g2.drawString("4. 'ENTER' untuk menyeleksi tanaman", x, y);
         }
 
         else if(menuScreenState == 2) {
             g2.setColor(Color.WHITE);
-            wp.plantMenu.drawList(g2);
+            wp.plantMenu.drawListPlant(g2);
         }
         else if(menuScreenState == 3) {
             g2.setColor(Color.WHITE);
-            wp.zombieMenu.drawList(g2);
+            wp.zombieMenu.drawListZombie(g2);
         }
     }
     public int centerX(String text) {
--- src/main/Game/Menu.java
+++ src/main/Game/Menu.java
@@ -2,6 +2,8 @@ package main.Game;
 
 import main.GUI.WindowPanel;
 import main.Game.ParentClass.Character;
+import main.Game.ParentClass.Plant;
+import main.Game.ParentClass.Zombie;
 
 import java.awt.*;
 import java.util.List;
@@ -15,23 +17,53 @@ public class Menu<T extends Character> {
         this.characterList = characterList;
     }
 
-    public void drawList(Graphics2D g2) {
+    public void drawListPlant(Graphics2D g2) {
         this.g2 = g2;
         int x = 50;
         int y = 30;
         for (T character : characterList) {
-            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 15F));
+            Plant plant = (Plant) character;
+            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 20F));
             g2.drawString(character.name, x, y);
             y += 20;
             g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 10F));
-            g2.drawString(character.description, x, y);
-            y += 25;
+            g2.drawString("Description: " + character.description, x, y);
+            y += 20;
+            g2.drawString("Cost: " + plant.cost, x, y);
+            y += 20;
+            g2.drawString("Health: " + character.health, x, y);
+            y += 20;
+            g2.drawString("Attack Damage: " + character.attack_damage, x, y);
+            y += 20;
+            g2.drawString("Attack Speed: " + character.attack_speed, x, y);
+            y += 20;
+            g2.drawString("Range: " + plant.range, x, y);
+            y += 20;
+            g2.drawString("Cooldown: " + plant.cooldown, x, y);
+            y += 30;
         }
     }
 
-    public int centerX(String text) {
-        int length = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-        int x = wp.screenWidth/2 - length/2;
-        return x;
+    public void drawListZombie(Graphics2D g2) {
+        this.g2 = g2;
+        int x = 50;
+        int y = 30;
+        for (T character : characterList) {
+            Zombie zombie = (Zombie) character;
+            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 20F));
+            g2.drawString(character.name, x, y);
+            y += 20;
+            g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 10F));
+            g2.drawString("Description: " + character.description, x, y);
+            y += 20;
+            g2.drawString("Health: " + character.health, x, y);
+            y += 20;
+            g2.drawString("Attack damage: " + character.attack_damage, x, y);
+            y += 20;
+            g2.drawString("Attack Speed: " + character.attack_speed, x, y);
+            y += 20;
+            g2.drawString("Aquatic: " + zombie.is_aquatic, x, y);
+            y += 30;
+        }
     }
 }
